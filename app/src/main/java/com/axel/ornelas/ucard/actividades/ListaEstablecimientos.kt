1ed54522692adf631f15391efbc31ef3413b017f
Axel L. Ornelas M <axel.lom17@gmail.com>
2022-05-09 03:20:36 -0700

    Creacion de la vista y la clase de los establecimientos

---
+++ app/src/main/java/com/axel/ornelas/ucard/actividades/ListaEstablecimientos.kt
@@ -0,0 +1,12 @@
+package com.axel.ornelas.ucard.actividades
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.axel.ornelas.ucard.R
+
+class ListaEstablecimientos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_establecimientos)
+    }
+}
