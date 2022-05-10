ac7edfdc158acfe473aeb2f5875b01d46e15a9b8
Axel L. Ornelas M <axel.lom17@gmail.com>
2022-05-10 04:32:08 -0700

    Se obtiene la cuenta para ingresar a la aplicación

--- app/src/main/java/com/axel/ornelas/ucard/actividades/MainActivity.kt
+++ app/src/main/java/com/axel/ornelas/ucard/actividades/MainActivity.kt
@@ -4,21 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.axel.ornelas.ucard.clases.Cuenta
+import com.axel.ornelas.ucard.clases.ManejoDeDatos
 import com.axel.ornelas.ucard.databinding.ActivityMainBinding
+import java.lang.Exception
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var cuentas: ArrayList<Cuenta>
+    private lateinit var manejadorDeDatos: ManejoDeDatos
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    }
-
-    private fun obtenerCuentas() {
-
-
+        manejadorDeDatos = ManejoDeDatos(applicationContext, resources, packageName, assets)
+        cuentas = manejadorDeDatos.obtenerCuentas()
     }
 
     /**
@@ -29,10 +33,23 @@ class MainActivity : AppCompatActivity() {
          * Por el momento se deja esto luego se debe cambiar para buscar el archivo
          */
         with(binding) {
-            if (correo.text.toString() == "hola" && contrasena.text.toString() == "1234") {
+            try {
+                val cuenta = cuentas.first {
+                    it.correo == correo.text.toString() && it.contrasena == contrasena.text.toString()
+                }
                 val intent = Intent(this@MainActivity, ListaCategorias::class.java)
+                intent.putExtra("nombreCuenta", cuenta.nombre)
+                intent.putExtra("idCuenta", cuenta.id)
                 startActivity(intent)
+            } catch (e: Exception) {
+                Toast.makeText(
+                    applicationContext,
+                    "No existe usuario con esas credenciales",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
+
+
         }
 
     }
