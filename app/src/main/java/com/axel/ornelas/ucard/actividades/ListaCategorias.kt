bebe6dc9b28396f3509111a9bea73df40cc125a2
Axel L. Ornelas M <axel.lom17@gmail.com>
2022-05-10 04:30:54 -0700

    Se obtiene el nombre de la cuenta y el id

--- app/src/main/java/com/axel/ornelas/ucard/actividades/ListaCategorias.kt
+++ app/src/main/java/com/axel/ornelas/ucard/actividades/ListaCategorias.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import com.axel.ornelas.ucard.adaptadores.CategoriasAdaptador
 import com.axel.ornelas.ucard.clases.Categoria
@@ -19,6 +20,9 @@ class ListaCategorias : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityListaCategoriasBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val idCuenta = intent.getIntExtra("idCuenta", -1)
+        if (idCuenta == -1)
+            finish()
         // Obtiene las categorias y las fotos y aquí se puede tener un archivo o una base de datos que recopile las categorias
         val categorias = arrayListOf(
             Categoria("Consultorios", obtenerImagen("categoria_consultorios")),
@@ -28,16 +32,23 @@ class ListaCategorias : AppCompatActivity() {
         )
         // Adaptador de categorias
         val categoriasAdaptador = CategoriasAdaptador(categorias)
+
         binding.listaCategoria.layoutManager = GridLayoutManager(applicationContext, 2)
         // Inicia la actividad dependiendo del restaurante
         categoriasAdaptador.onClickListener = View.OnClickListener { v ->
             val pos = binding.listaCategoria.getChildAdapterPosition(v)
             val categoria = categorias[pos].tipo
-            val intent = Intent(this@ListaCategorias, ListaEstablecimientos::class.java)
+            val intent = Intent(this, ListaEstablecimientos::class.java)
             intent.putExtra(PARAM1, categoria)
             startActivity(intent)
         }
         binding.listaCategoria.adapter = categoriasAdaptador
+        val cuentaNombre = intent.getStringExtra("nombreCuenta")
+        Toast.makeText(
+            applicationContext,
+            "Bienvenido a tu cuenta: $cuentaNombre",
+            Toast.LENGTH_SHORT
+        ).show()
     }
 
     /**
