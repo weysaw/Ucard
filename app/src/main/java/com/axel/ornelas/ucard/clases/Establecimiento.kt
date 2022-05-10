3b2436c81e9c9c317602ad742fbfbcc9c52fa627
Axel L. Ornelas M <axel.lom17@gmail.com>
2022-05-09 20:24:54 -0700

    se cambio como se genera un nuevo cupon

--- app/src/main/java/com/axel/ornelas/ucard/clases/Establecimiento.kt
+++ app/src/main/java/com/axel/ornelas/ucard/clases/Establecimiento.kt
@@ -1,27 +1,24 @@
 package com.axel.ornelas.ucard.clases
 
-import java.util.*
+import java.io.Serializable
 import kotlin.collections.ArrayList
 
 class Establecimiento(
     val nombre: String,
     val direccion: String,
+    val categoria: String,
+    val logo: Int,
     // Por el momento lo estoy pensando como un arreglo de direcciones pero puede cambiar
-    val fotos: ArrayList<String>,
-    val cupones: ArrayList<Cupon>
-) {
+    val fotos: ArrayList<Int>,
+): Serializable {
+    val cupones: ArrayList<Cupon> = arrayListOf()
     private var cantCuponesCreados: Int = 0
     /**
      * Genera un nuevo cupon con el dato de la promocion
      */
-    fun generarNuevoCupon(promocion: String) {
+    fun generarNuevoCupon(promocionImagen: Int, promocion: String) {
         cantCuponesCreados++
-        // Obtiene la fecha actual
-        val fechaActual = Calendar.getInstance()
-        val fechaVencimiento = fechaActual.clone() as Calendar
-        // Obtiene la fecha de vencimiento
-        fechaVencimiento.add(Calendar.MONTH, 1) // Zero-based months
-        val nuevoCupon = Cupon(cantCuponesCreados, nombre, promocion, fechaActual, fechaVencimiento)
+        val nuevoCupon = Cupon(cantCuponesCreados, promocionImagen, promocion)
         cupones += nuevoCupon
     }
 }
