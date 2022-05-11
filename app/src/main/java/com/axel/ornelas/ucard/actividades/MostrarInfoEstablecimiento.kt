d84d63c482596596c28d456fe96fa4e8f48074e3
Axel L. Ornelas M <axel.lom17@gmail.com>
2022-05-10 22:08:05 -0700

    Agrega el slider en la ultima parte y se documento

--- app/src/main/java/com/axel/ornelas/ucard/actividades/MostrarInfoEstablecimiento.kt
+++ app/src/main/java/com/axel/ornelas/ucard/actividades/MostrarInfoEstablecimiento.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.axel.ornelas.ucard.adaptadores.CuponesAdaptador
+import com.axel.ornelas.ucard.adaptadores.SliderAdapter
 import com.axel.ornelas.ucard.clases.Establecimiento
 import com.axel.ornelas.ucard.clases.ManejoDeDatos
 import com.axel.ornelas.ucard.databinding.ActivityMostrarInfoEstablecimientoBinding
@@ -17,8 +18,10 @@ class MostrarInfoEstablecimiento : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMostrarInfoEstablecimientoBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        // Obtiene el establecimiento de la actividad pasada
         val establecimiento = intent.getSerializableExtra("establecimiento") as Establecimiento
         binding.nombreEstablecimiento.text = establecimiento.nombre
+        //Carga el logo con glide
         Glide.with(applicationContext)
             .load(establecimiento.logo)
             .fitCenter()
@@ -26,10 +29,18 @@ class MostrarInfoEstablecimiento : AppCompatActivity() {
         val idCuenta = intent.getIntExtra("idCuenta", -1)
         if (idCuenta == -1)
             finish()
+        // Se utiliza para reclamar los cupones
         val manejoDeDatos = ManejoDeDatos(applicationContext, resources, packageName, assets)
         val cuponesAdaptador = CuponesAdaptador(establecimiento.cupones, idCuenta, manejoDeDatos)
         binding.listaCupones.layoutManager = LinearLayoutManager(applicationContext)
         binding.listaCupones.adapter = cuponesAdaptador
 
+        val sliderAdapter = SliderAdapter(establecimiento.fotos)
+        with(binding.slider) {
+            setSliderAdapter(sliderAdapter)
+            scrollTimeInSec = 3
+            isAutoCycle = true
+            startAutoCycle()
+        }
     }
 }
