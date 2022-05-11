19915c370466e7f07ec4567e46b20a13097bb281
Axel L. Ornelas M <axel.lom17@gmail.com>
2022-05-10 19:45:42 -0700

    Se eliminan metodos y propiedades innecesarias

--- app/src/main/java/com/axel/ornelas/ucard/clases/Cupon.kt
+++ app/src/main/java/com/axel/ornelas/ucard/clases/Cupon.kt
@@ -8,25 +8,7 @@ import java.util.*
  */
 class Cupon(
     val id: Int,
+    val idEstablecimiento: Int,
     val promocionImagen: Int,
     val descripcion: String,
-    val fechaCreacion: Calendar = Calendar.getInstance(),
-    val fechaVencimiento: Calendar = fechaCreacion.clone() as Calendar
-): Serializable {
-
-    init {
-        // Obtiene la fecha de vencimiento
-        fechaVencimiento.add(Calendar.MONTH, 1)
-    }
-
-    fun verificarValidezCupon(): Boolean {
-        val fechaActual = Calendar.getInstance()
-        return if (fechaVencimiento <= fechaActual)
-            true
-        else {
-            //Verificar que en el archivo que el cupon existe
-            false
-        }
-
-    }
-}
+) : Serializable
